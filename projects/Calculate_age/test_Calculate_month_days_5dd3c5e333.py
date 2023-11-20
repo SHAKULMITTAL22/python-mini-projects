764480d6c880b73c97f6ad9167bd4fed519b7745
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2023-11-20 18:21:35 +0530

    Unit test generated by RoostGPT
    Using AI Model gpt-4-1106-preview

---
+++ projects/Calculate_age/test_Calculate_month_days_5dd3c5e333.py
@@ -0,0 +1,8 @@
+# calculate.py
+def month_days(month, is_leap_year):
+    # Function implementation here
+    pass
+
+if __name__ == '__main__':
+    name = input("input your name: ")
+    # More code that uses the input
